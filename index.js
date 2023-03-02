f5c5e0305dc149d8f1ca4e468eb5690308e55998
stapler-codes <123899436+stapler-codes@users.noreply.github.com>
2023-03-02 17:33:07 +0200

    hi

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ timer = setInterval(function() {
         document.getElementById("x_bang").style.left = "28vw"
         document.body.style.width = '60vw';
         document.getElementById("all_bang").style.left = "-2.5vw"
+        document.getElementById("all_bang").style.width = "5rem"
     }
 }, 100)
